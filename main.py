fdb3493336df15a9637adcead652175927df3366
MClose23 <123100717+MClose23@users.noreply.github.com>
2024-02-08 18:25:36 -0600

    Attempt 1.0 to commit
    yuhhhhh

--- main.py
+++ main.py
@@ -2,15 +2,31 @@
 #as a string
 #January -> 1 (as a string)
 #YOU MAY USE THIS FUNCTION IF YOU WANT TO OR YOU MAY REMOVE IT
-def parse_month(month):
-    pass
+def parse_date(string_date):
 
+    if string_date != "-1":
+        #items = string_date.split(", ")
+        month, day, year = string_date.split(' ')
+        day = int(day.replace("," , ""))
+        day = '{:02d}'.format(day)
+        month_dictionary = { "January" : "01", "February" : "02", "March" : "03", "April" : "04", "May" : "05", "June" : "06", "July" : "07", "August" : "08", "September" : "09", "October" : "10", "November" : "11", "December" : "12"  }
+        month = month_dictionary.get(month,"01")
+        separator = '/'
+        reg_date = separator.join((month, day, year))
+        return reg_date
+
+    
+        
 #REMOVE PASS AND FIX THIS FUNCTION
 #parse_date function should return the date formated as MM/DD/YYYY
 #DO NOT REMOVE THIS FUNCTION
-def parse_date(user_string):
-    pass
+#def parse_date(user_string): 
+    #user_string.replace(' ', '/')
 
 #REMOVE PASS AND YOUR CODE GOES HERE
 if __name__ == '__main__':
-    pass
+    string_date = input()
+    print(reg_date)
+    
+
+    
